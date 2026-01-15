8f2ac96c6987f9d46e2054b70a2c1781ada3271d
Vipin <vipinbhardwal286@gmail.com>
2026-01-15 21:56:55 +0530

    Responsive designing started

--- frontend/src/Components/DescriptionBox/Description.jsx
+++ frontend/src/Components/DescriptionBox/Description.jsx
@@ -1,21 +1,21 @@
-import React from 'react'
-import './DescriptionBox.css'
-const Description = () => {
-  return (
-    <div className='description-box'>
-        <div className="description-box-navigator">
-            <div className="description-box-nav-box">Description</div>
-            <div className="description-box-nav-box fade">Reviews (122)</div>
+    import React from 'react'
+    import './DescriptionBox.css'
+    const Description = () => {
+    return (
+        <div className='description-box'>
+            <div className="description-box-navigator">
+                <div className="description-box-nav-box">Description</div>
+                <div className="description-box-nav-box fade">Reviews (122)</div>
 
+            </div>
+            <div className="description-box-description">
+                <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Sequi impedit porro ullam laboriosam dicta doloremque repudiandae vitae enim ex distinctio. Nesciunt a repudiandae vitae totam suscipit molestias hic, nihil repellendus in omnis reprehenderit corporis eligendi quae dolor voluptatum eos maiores?</p>
+                <p>
+                    Lorem ipsum dolor sit, amet consectetur adipisicing elit. Vero maiores, quos quisquam incidunt quidem explicabo velit perferendis inventore tempore eius.
+                </p>
+            </div>
         </div>
-        <div className="description-box-description">
-            <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Sequi impedit porro ullam laboriosam dicta doloremque repudiandae vitae enim ex distinctio. Nesciunt a repudiandae vitae totam suscipit molestias hic, nihil repellendus in omnis reprehenderit corporis eligendi quae dolor voluptatum eos maiores?</p>
-            <p>
-                Lorem ipsum dolor sit, amet consectetur adipisicing elit. Vero maiores, quos quisquam incidunt quidem explicabo velit perferendis inventore tempore eius.
-            </p>
-        </div>
-    </div>
-  )
-}
+    )
+    }
 
-export default Description
+    export default Description
--- frontend/src/Pages/LoginSignup.jsx
+++ frontend/src/Pages/LoginSignup.jsx
@@ -1,27 +1,31 @@
-import React from 'react'
-import './CSS/loginSignup.css'
+import React from "react";
+import "./CSS/loginSignup.css";
 const LoginSignup = () => {
   return (
-    <div className='login-signup'>
-      <div className="login-signup-container">
-        <h1>Sign up</h1>
-        <div className="login-signup-fields">
-          <input type="text" placeholder='Your Name' />
-          <input type="email" placeholder='Email Address' />
-          <input type="password" placeholder='Password' />
-        </div>
-        <button>Continue</button>
-        <p className="login-signup-login">
-          Already have an acount?
-          <span>Login here</span>
-        </p>
-        <div className="login-signup-agree">
-          <input type="checkbox" name='' id='' />
-          <p>By continuing, I agree to the terms of use & privacy policy</p>
+    <div className="login-signup">
+      <div className="login-signup-overlay">
+        <div className="login-signup-container">
+          <div className="login-signup-container">
+            <h1>Sign up</h1>
+            <div className="login-signup-fields">
+              <input type="text" placeholder="Your Name" />
+              <input type="email" placeholder="Email Address" />
+              <input type="password" placeholder="Password" />
+            </div>
+            <button>Continue</button>
+            <p className="login-signup-login">
+              Already have an acount?
+              <span>Login here</span>
+            </p>
+            <div className="login-signup-agree">
+              <input type="checkbox" name="" id="" />
+              <p>By continuing, I agree to the terms of use & privacy policy</p>
+            </div>
+          </div>
         </div>
       </div>
     </div>
-  )
-}
+  );
+};
 
-export default LoginSignup
+export default LoginSignup;
